1a87f446455425aaa377f01b13c07947b13b41f7
Marlon <marlondametto@hotmail.com>
2021-03-21 20:28:38 -0300

    21-03-2021 Mostra shape dos dados carregados.

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-import subprocess
 import logging
-from flask import Flask,request, render_template
+import subprocess
+
+import matplotlib.pyplot as plt
+import numpy
+
+import pandas
+from flask import (Flask, jsonify, redirect, render_template, request, session,
+                   url_for)
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -19,7 +25,7 @@ def playsound():
     MyOut1 = subprocess.call(f'''termux-volume music 15''', shell=True)
     MyOut = subprocess.call(f'''termux-tts-speak {text}''', shell=True)
     #return render_template("index.html")
-    return home()
+    return redirect(url_for('main'))
     #return welcome('Marlon')
 
 @app.route('/sms', methods=['GET','POST'])
@@ -34,7 +40,17 @@ def sendsms():
     
     MyOut = subprocess.call(f'''termux-sms-send -n {number} {text}''', shell=True)  
     
-    return home()
+    return redirect(url_for('main'))
 
+@app.route('/loadData')
+def loadData():
+    try:
+        name = request.args.get('name')        
+        if name == 'simples':    
+            base = pandas.read_csv('./static/cars.csv')
+            logging.error('name: {}'.format(base.head().to_json()))
+        return base.head().to_json()        
+    except Exception as e:
+        return redirect(url_for('main'))
 if __name__ =='__main__':
     app.run(debug=True)
