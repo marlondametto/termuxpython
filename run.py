31983c18125f5a81a97e98fb537b38bed8ad3b4d
Marlon <marlondametto@hotmail.com>
2021-09-12 16:23:18 -0300

    Gps test

--- run.py
+++ run.py
@@ -161,7 +161,8 @@ def getLocation(param):
         logging.warning("conexão ao sqlite criada")
         while True:            
 
-            myOut = subprocess.check_output(f'''termux-location -p network''', shell=True)
+            # myOut = subprocess.check_output(f'''termux-location -p network''', shell=True)
+            myOut = subprocess.Popen(f'''termux-location -p network''', shell=True)
             logging.warning("Tipo de dado: {}".format(type(myOut)))
             logging.warning("termux-location: {}".format(myOut))  
             transformed=myOut.decode('utf-8')
